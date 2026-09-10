f80e3ebd74a8084ab530f1086041cb776f6b732b
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2026-09-10 20:53:59 +0200

    [ CORRECTION PWM 10KHZ COMMANDABLE AVEC 10 BITS] le pwm que j'avais d'abord réalisé était sur 13 bits, mais en utilisant un prédiviseur on a pu le réduire à 10 bits. le schéma de fonctionnement est :  clk_50MHz --> (diviseur de fréquence à clk_10MHz ) --> ( compteur sur  10000) pour avoir pwm 10kHz.  Donc il faut deux process, un process pour faire un diviseur de  fréquence et un autre process pour compter à partir de cette horloge.

--- VHDL/pwm hierachique/projet1_led.vhd
+++ VHDL/pwm hierachique/projet1_led.vhd
@@ -5,14 +5,15 @@ use ieee.numeric_std.all;
   -- fpwm = 50M/5000 = 10kHz 
 entity projet1_led is 
     generic(
-        max : natural := 5000
+        max : natural := 1000;
+		  pres_scaler : natural :=5
     );
     port(
         clk : in  std_logic;
         rst : in  std_logic;
         sens_select : in std_logic ;
         sens : out std_logic ;
-        rapport : in std_logic_vector(12 downto 0) ;
+        rapport : in std_logic_vector(9 downto 0) ;
         pwm : out std_logic
     );     
 end projet1_led;
@@ -21,39 +22,55 @@ architecture beh of projet1_led is
 
     signal counter : integer range 0 to max-1 := 0;
     signal comp    : integer range 0 to max := 0;
-    
+    signal div_clk : std_logic; 
 
 begin
 
     -- Génération PWM
      comp <= to_integer(unsigned(rapport));
-    process(clk, rst)
+    process(div_clk, rst)
+	 variable tick_div : integer range 0 to pres_scaler-1:=0; 
     begin
 
         if rst = '0' then
-
+            tick_div := 0;
             counter <= 0;
             pwm<= '0';
-
-        elsif rising_edge(clk) then
-
-            if counter = max-1 then
-                counter <= 0;
-            else
-                counter <= counter + 1;
-            end if;
-
-            if counter < comp then
-                pwm <= '1';
-            else
-                pwm <= '0';
-            end if;
-
-        end if;
+        elsif  rising_edge(div_clk) then 
+		         if counter=max-1 then 
+					counter <= 0 ;
+					else
+					counter <= counter + 1 ;
+					end if ; 
+			 if counter < comp then
+              pwm <= '1';
+          else
+              pwm <= '0';
+         end if;
+	   end if ; 		
+      
 
     end process;
-     -- sens de rotation moteur
-     sens <= sens_select ;
+	 
+process(clk, rst)
+    variable tick : integer range 0 to pres_scaler-1 := 0;
+begin
+    if rst = '0' then
+        div_clk <= '0';
+        tick := 0;
+    elsif rising_edge(clk) then
+        if tick = pres_scaler-1 then
+            div_clk <='1';
+            tick := 0;
+        else
+            tick := tick + 1;
+				  div_clk <= '0';
+        end if;
+    end if;
+end process;
+
+-- sens de rotation moteur
+sens <= sens_select;
 
 end beh;
 
--- VHDL/pwm hierachique/top.vhd
+++ VHDL/pwm hierachique/top.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
   
 entity top is 
     generic(
-        max : natural := 5000
+        max : natural := 1000
     );
     port(
         clk : in  std_logic;
@@ -14,20 +14,20 @@ entity top is
 		
 		 sens_choix : in std_logic ;
 		 sens_rotation : out std_logic ; 
-         pwm_sortie : out std_logic 
+       pwm_sortie : out std_logic 
     );     
 end top;
 
 architecture beh of top is 
     signal comp    : integer range 0 to max := 0;
-	  signal compteur_comp : std_logic_vector(12 downto 0) ;
+	  signal compteur_comp : std_logic_vector(9 downto 0) ;
 	 Component projet1_led 
   port(
         clk : in  std_logic;
         rst : in  std_logic;
         sens_select : in std_logic ;
         sens : out std_logic ;
-        rapport : in std_logic_vector(12 downto 0) ;
+        rapport : in std_logic_vector(9 downto 0) ;
         pwm : out std_logic
     );    
  end Component ;
@@ -35,14 +35,14 @@ architecture beh of top is
 begin
 
     -- Choix du rapport cyclique
-	-- le multiplexeur 4 vers 1 ici permet de décider du rapport cyclique qui va sur le pwm 
-       comp <= 500  when cs = "00" else  -- 10 %
-       1500 when cs = "01" else     -- 30 %
-        2500 when cs = "10" else     -- 50 %
-        4000;                        -- 80 %  
+	-- le multiplexeur 4 vers 1 ici permet de decider du rapport cyclique qui va sur le pwm 
+       comp <= 200  when cs = "00" else  -- 20 %
+       300 when cs = "01" else     -- 30 %
+       500 when cs = "10" else     -- 50 %
+       800;                        -- 80 %  
 
-    -- Génération PWM
-	 compteur_comp <= std_logic_vector(to_unsigned(comp, 13)); 
+    -- Generation PWM
+	 compteur_comp <= std_logic_vector(to_unsigned(comp, 10)); 
 	 pwm_map : projet1_led PORT MAP (
        clk => clk,
 	   rst => rst,
