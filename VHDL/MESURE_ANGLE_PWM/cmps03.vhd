0cc76486d2218fe4d8cfb01202fd9f07a2aa2d2e
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2026-09-24 11:23:55 +0200

    Code avec un compteur de 1ms

--- VHDL/MESURE_ANGLE_PWM/cmps03.vhd
+++ VHDL/MESURE_ANGLE_PWM/cmps03.vhd
@@ -5,14 +5,15 @@ use ieee.numeric_std.all;
 entity cmps03 is
     generic (
         CLK_FREQ_HZ : natural := 50000000;
-        period      : natural := 102
+        period      : natural := 65 
     );
     port (
         clk           : in  std_logic;
-        rst           : in  std_logic;      -- actif bas
+        rst           : in  std_logic;     
         in_pwm_compas : in  std_logic;
-        continu       : in  std_logic;      -- non utilisé dans cette FSM
-        start_stop    : in  std_logic;      -- non utilisé dans cette FSM
+        continu       : in  std_logic;     
+        start_stop    : in  std_logic;  
+          out_oscillo   : out std_logic ;            
         data_valid    : out std_logic;
         data_compas   : out std_logic_vector(8 downto 0)
     );
@@ -37,15 +38,18 @@ architecture mealy of cmps03 is
     signal fin_tim     : std_logic := '0';
     signal time_cont   : integer range 0 to period := 0;
     signal window_done : std_logic;
+     signal clk_pwm : std_logic :='0';
 begin
     rising_pwm  <= '1' when sync(2 downto 1) = "01" else '0';
     falling_pwm <= '1' when sync(2 downto 1) = "10" else '0';
     level_pwm   <= sync(1);
     window_done <= '1' when (fin_tim = '1' and time_cont = period - 1) else '0';
+     
+        -- sortie Oscilloscope
+       out_oscillo <= in_pwm_compas ; 
 
-    ------------------------------------------------------------------
-    -- PROCESS 1/3 (obligatoire) : registre d'etat
-    ------------------------------------------------------------------
+    
+    -- PROCESS : sequentiel
     p_state_reg : process(clk, rst)
     begin
         if rst = '0' then
@@ -55,9 +59,9 @@ begin
         end if;
     end process;
 
-    ------------------------------------------------------------------
-    -- PROCESS 2/3 (obligatoire) : logique d'etat suivant (combinatoire)
-    ------------------------------------------------------------------
+  
+    -- PROCESS : combinatoire
+    
     p_next_state : process(state, rising_pwm, falling_pwm, window_done)
     begin
         next_state <= state;
@@ -77,10 +81,9 @@ begin
         end case;
     end process;
 
-    ------------------------------------------------------------------
-    -- PROCESS 3/3 (obligatoire) : sortie de Mealy (combinatoire,
-    -- depend de l'etat ET d'une condition d'entree)
-    ------------------------------------------------------------------
+   
+      -- pour valider la data 
+      
     p_output : process(state, window_done)
     begin
         data_valid <= '0';
@@ -89,9 +92,9 @@ begin
         end if;
     end process;
 
-    ------------------------------------------------------------------
-    -- Processus additionnels : synchroniseur + chemin de donnees
-    ------------------------------------------------------------------
+    
+    -- Process qui prend la donnée et les décales 
+     
     p_sync : process(clk, rst)
     begin
         if rst = '0' then
@@ -101,6 +104,8 @@ begin
         end if;
     end process;
 
+     -- process sortie 
+     
     p_datapath : process(clk, rst)
         variable a : integer range 0 to 511;
     begin
@@ -108,17 +113,11 @@ begin
             cmpt_tick   <= 0;
             angle       <= 0;
             data_out    <= (others => '0');
-            ms_cmpt     <= 0;
-            fin_tim     <= '0';
+          
             time_cont   <= 0;
             data_compas <= (others => '0');
         elsif rising_edge(clk) then
-            if ms_cmpt = MS_CYC - 1 then
-                ms_cmpt <= 0; fin_tim <= '1';
-            else
-                ms_cmpt <= ms_cmpt + 1; fin_tim <= '0';
-            end if;
-
+           
             case state is
                 when S_WAIT_EDGE =>
                     if rising_pwm = '1' then
@@ -132,12 +131,14 @@ begin
                         if a > 359 then a := 359; end if;
                         data_out <= std_logic_vector(to_unsigned(a, 9));
                     elsif level_pwm = '1' then
+                          
                         if cmpt_tick = TICK_CYC - 1 then
                             cmpt_tick <= 0;
                             if angle < 511 then angle <= angle + 1; end if;
                         else
                             cmpt_tick <= cmpt_tick + 1;
                         end if;
+                                
                     end if;
 
                 when S_WAIT_WINDOW =>
@@ -150,4 +151,26 @@ begin
             end case;
         end if;
     end process;
+     
+      -- compteur de 1ms 
+      
+      process(clk, rst)
+        variable cmpt : integer range 0 to MS_CYC - 1 := 0;
+    begin
+        if rst = '0' then
+            cmpt    := 0;
+            fin_tim <= '0';
+        elsif rising_edge(clk) then
+            if cmpt = MS_CYC - 1 then
+                cmpt    := 0;
+                fin_tim <= '1';
+            else
+                cmpt    := cmpt + 1;
+                fin_tim <= '0';
+            end if;
+        end if;
+    end process;
+     
+     -- horloge pwm 
+
 end architecture;
