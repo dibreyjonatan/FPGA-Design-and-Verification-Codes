57583938340f068e7bb05c2e7cb23006fa047c2a
dibreyjonatan <kamdadibreyjonatan@gmail.com>
2026-09-26 06:44:30 +0200

    [TEST: VALIDATED IN SIMULATION]    pour la simulation j'ai fais 3 mesures en monocoup et 3 mesures en continu. j'ai mis expressement un délai de 1000ms dans le process afin d'influencer mon pwm et voir comment mon dut réagit, et il a plutot bien réagit.

--- VHDL/MESURE_ANGLE_PWM/tb_cmps03.vhd
+++ VHDL/MESURE_ANGLE_PWM/tb_cmps03.vhd
@@ -18,6 +18,7 @@ architecture sim of tb_cmps03 is
     signal data_valid  : std_logic;
     signal data_compas : std_logic_vector(8 downto 0);
     signal done        : boolean := false;
+	signal continu_t : std_logic :='0' ; 
 begin
 
     clk <= not clk after CLK_PERIOD / 2 when not done else clk;
@@ -28,11 +29,11 @@ begin
             clk           => clk,
             rst           => rst,
             in_pwm_compas => pwm,
-            continu       => '1',
+            continu       => continu_t,
             start_stop    => '0',
             data_valid    => data_valid,
             data_compas   => data_compas,
-			cmd => '0' 
+			cmd => '1' 
         );
 
     stim : process
@@ -44,6 +45,7 @@ begin
             wait for 1 ms + a * 100 us;   -- 1 ms d'offset + 100 us/degré
             pwm <= '0';
             wait for 65 ms;               -- état bas
+		--	wait for 935 ms;  -- pour que 65+935 = 1s par chaque mesure   
         end procedure;
     begin
         rst <= '0';
@@ -53,11 +55,13 @@ begin
 
         for i in ANGLES'range loop
             wait for (i * 137 + 53) * 1 ns;     -- phase asynchrone vis-à-vis de clk
-
+            if i=3 then 
+			   continu_t <= not continu_t ; 
+			end if ;    
             for k in 1 to 3 loop
                 pulse(ANGLES(i));
             end loop;
-
+            	wait for 1000 ms;
             wait until rising_edge(data_valid) for 110 ms;
             wait for CLK_PERIOD / 2;
 
@@ -71,7 +75,7 @@ begin
                     severity error;
                 errors := errors + 1;
             else
-                report "OK : angle " & integer'image(ANGLES(i)) & " correctement mesuré";
+                report "OK : angle " & integer'image(ANGLES(i)) & " correctement mesurer";
             end if;
         end loop;
 
